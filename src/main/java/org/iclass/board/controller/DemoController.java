0ac9ee1d1bf59c7b75407713c1d9a522045789c8
GAGmanga <gatolindo@naver.com>
2024-09-30 16:08:36 +0900

    2_demo contoller, demo dto에 코드 작성

--- src/main/java/org/iclass/board/controller/DemoController.java
+++ src/main/java/org/iclass/board/controller/DemoController.java
@@ -4,6 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
+@Slf4j
 public class DemoController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
 }
--- src/main/java/org/iclass/board/dto/DemoDTO.java
+++ src/main/java/org/iclass/board/dto/DemoDTO.java
@@ -1,4 +1,10 @@
 package org.iclass.board.dto;
 
+import java.time.LocalDateTime;
+
 public class DemoDTO {
+
+    private String demoName;
+    private int demoAge;
+    private LocalDateTime demoDate;
 }
